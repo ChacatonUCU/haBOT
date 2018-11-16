490aafb61e43018127ad473d386b1d523366a596
Артемій <44300059+Katerunner@users.noreply.github.com>
2018-11-16 14:41:51 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,41 +1,50 @@
 from telegram.ext import Updater, CommandHandler
-
-
+from telegram.ext import Filters, MessageHandler
+import time
+lstgood = []
+lstbad = []
+i = 0
 def start(bot, update):
     greetings = 'Hello {}\n'.format(update.message.from_user.first_name)
     greetings += "I am ready to start."
     update.message.reply_text(greetings)
 
+def saveanswer(bot, update):
+    global i
+    if i == 1:
+        if len(lstgood) < 3:
+            lstgood.append(update.message.text)
+        elif len(lstbad) < 3:
+            lstbad.append(update.message.text)
+        else:
+            pass
+        if len(lstgood) == 3 and len(lstbad) == 0:
+            update.message.reply_text("Now type bad films")
+        if len(lstgood) == 3 and len(lstbad) == 3:
+            i = 0
+            update.message.reply_text("All films are entered!")
+    else:
+        pass
+
+
+def testik(bot, update):
+    update.message.reply_text("Good films" + str(lstgood))
+    update.message.reply_text("Bad films" + str(lstbad))
 
 def advice(bot, update):
-    def film_append_helper(lst):
-        iterator,used = 0,[]
-        while iterator < 3:
-            # text = messages
-            if not text.startwith("/"):
-                lst_best.append(text)
-                iterator += 1
-                used.append(text)
-                print(lst)
-
-    
-    lst_best, lst_worst = [], []
+    lstgood.clear()
+    lstbad.clear()
+    global i
+    i = 1
     update.message.reply_text("Please enter 3 your favourite films")
-    film_append_helper(lst_best)
-    update.message.reply_text("And now please enter 3 worst films")
-    film_append_helper(lst_worst)
-    print(lst_best)
-    print(lst_worst)
-
-# def hello(bot, update):
-#     update.message.reply_text(
-#         'Hello {}'.format(update.message.from_user.first_name))
+    # update.message.reply_text("And now please enter 3 worst films")
 
 
 updater = Updater('787053848:AAF_lcSiJTT0AAIWKX5KmAwbHfxVcTh_KwM')
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('advice', advice))
-
+updater.dispatcher.add_handler(MessageHandler(Filters.text, saveanswer))
+updater.dispatcher.add_handler(CommandHandler('test', testik))
 updater.start_polling()
 updater.idle()
