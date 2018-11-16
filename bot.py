65978bc57e33eb2dbf75f86b26828e0b717a3893
Jarko Umbrelluck <umbrelluck@gmail.com>
2018-11-16 11:01:47 +0200

    Added some default behaviour

--- bot.py
+++ bot.py
@@ -1,14 +1,20 @@
 from telegram.ext import Updater, CommandHandler
 
 
-def hello(bot, update):
-    update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name))
+def start(bot, update):
+    greetings = 'Hello {}\n'.format(update.message.from_user.first_name)
+    greetings += "Are you ready to start?"
+    update.message.reply_text(greetings)
+
+
+# def hello(bot, update):
+#     update.message.reply_text(
+#         'Hello {}'.format(update.message.from_user.first_name))
 
 
 updater = Updater('787053848:AAF_lcSiJTT0AAIWKX5KmAwbHfxVcTh_KwM')
 
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler('start', start))
 
 updater.start_polling()
 updater.idle()
